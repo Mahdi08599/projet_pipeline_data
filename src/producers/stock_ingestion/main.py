6161ab4c726804f8bc0cfc90efbc23cec99b16ba
Mahdi08599 <Mahdi.BENARFI@esprit.tn>
2026-01-20 11:35:02 +0100

    Update main.py

--- src/producers/stock_ingestion/main.py
+++ src/producers/stock_ingestion/main.py
@@ -10,19 +10,27 @@ from common.logger import get_logger
 ticker = "AAPL"
 producer = get_producer()
 logger = get_logger("StockProducer-AAPL")
+last_price = None
 
 logger.info("Démarrage du service d'ingestion...")
 
 try:
     while True:
         data = yf.Ticker(ticker).history(period='1d', interval='1m').iloc[-1]
-        message = {
-            "symbol": ticker,
-            "price": float(data['Close']),
-            "timestamp": str(data.name)
-        }
-        producer.send('stock_prices', value=message)
-        logger.info(f"Donnée boursière envoyée avec succès : {message['price']} USD")
+        current_price = float(data['Close'])
+        
+        if current_price != last_price:
+            message = {
+                "symbol": ticker,
+                "price": current_price,
+                "timestamp": str(data.name)
+            }
+            producer.send('stock_prices', value=message)
+            logger.info(f"Donnée boursière envoyée avec succès : {message['price']} USD")
+            last_price = current_price
+        else:
+            logger.info(f"Prix inchangé ({current_price} USD), pas d'envoi.")
+        
         time.sleep(60)
 except KeyboardInterrupt:
     logger.info("Arrêt du service demandé par l'utilisateur.")
